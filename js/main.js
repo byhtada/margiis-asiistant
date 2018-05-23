04c105312c02edf53c254bc97a1d9890080008bc
byhtada <byhtada@gmail.com>
2018-05-23 10:44:44 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    VK.init({
+        apiId: 6487614
+    });
+    VK.Widgets.Auth("vk_auth", {"onAuth":"function(data) {alert('user '+data['uid']+' authorized');}"});
+
     if (!navigator.cookieEnabled) {
         alert('Включите cookie для комфортной работы');
     }
