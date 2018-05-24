6cd078d3c1295bcdfebdbe820c103830d83ff0cd
byhtada <byhtada@gmail.com>
2018-05-24 13:26:18 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 window.onload = function () {
-
+    var button = $ipsp.get("button");
+    button.setHost("api.fondy.eu");
+    button.setProtocol("https");
+    button.setMerchantId(1409532);
+    button.setAmount("","RUB",false);
+    button.setResponseUrl("https://byhtada.github.io/hyls_client/");
+    button.addParam("lang","ru");
+    button.addParam("order_desc","Платеж за товары  ");
+    var url = button.getUrl();
+    $ipsp("checkout").config({
+        "wrapper": "#checkout",
+        "styles": {
+            "body": {
+                "overflow": "hidden"
+            }
+        }
+    }).scope(function () {
+        this.width("100%");
+        this.height(480);
+        this.action("resize", function (data) {
+            this.setCheckoutHeight(data.height);
+        });
+        this.loadUrl(url);
+    });
 };
 
 
