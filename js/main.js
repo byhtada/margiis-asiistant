50b231dc6f1768be3a501ee0c4865a777edcd066
Alex <byhtada@gmail.com>
2018-06-20 19:38:45 +0300

    1

--- js/main.js
+++ js/main.js
@@ -2240,6 +2240,7 @@ $( document ).ready(function() {
         hide_all_in_user();
         hide_all_in_admin();
         $("#nav_bar")     .hide();
+        $("#nav_bar_admin")     .hide();
         $("#page_login")     .show();
 
     });
@@ -2314,7 +2315,24 @@ $( document ).ready(function() {
         var user_password   = $('#field_user_reg_password').val();
 
 
-        if (user_phone != "" && user_password != "" && user_email != "" && user_password.length >= 4 && isValidEmailAddress(user_email)) {
+        var user_phone_check    = false;
+        var user_password_check = false;
+        var user_email_check    = false;
+
+        if (user_phone == "") {
+            user_phone_check = true;
+            $('#reg_hand_error_phone').show();
+        }
+        if (user_password == "" || user_password.length <= 4 ) {
+            user_password_check = true;
+            $('#reg_hand_error_password').show();
+        }
+        if (user_email == "" || isValidEmailAddress(user_email) == false) {
+            user_email_check = true;
+            $('#reg_hand_error_email').show();
+        }
+
+        if (user_phone_check && user_password_check && user_email_check) {
             var person  = {
                 first_name:  user_name,
                 phone:       user_phone,
@@ -2356,6 +2374,14 @@ $( document ).ready(function() {
         var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
         return pattern.test(emailAddress);
     }
+
+    $('#btn_support_show_currency').click(function(){
+        if ($('#support_currency').is(":visible")) {
+            $('#support_currency').hide();
+        } else {
+            $('#support_currency').show();
+        }
+    });
 
     $('#btn_reg_show_currency').click(function(){
         if ($('#reg_currency').is(":visible")) {
