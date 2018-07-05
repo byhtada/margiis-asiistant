d57f744f781068e70c129e27e81473545fbe1228
Alex <byhtada@gmail.com>
2018-07-05 13:34:06 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1554,7 +1554,13 @@ $( document ).ready(function() {
                     if (data.marafon_day < -998) {
                         $('#reg_marafon_create_password').hide();
                         $('#page_marafon_reg').show();
-                        data.user.program_type.includes("21day") ? $('#reg_marafon_mini').show() : $('#reg_marafon_detox').show()
+                        if (data.user.program_type.includes("21day")) {
+                            $('#reg_marafon_mini').show();
+                            console.log("mini");
+                        } else {
+                            $('#reg_marafon_detox').show();
+                            console.log("detox");
+                        }
 
                     } else if  (data.marafon_day > -998 && data.marafon_day < 1){
                         console.log("wait");
