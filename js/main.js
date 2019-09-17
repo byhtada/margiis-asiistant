e6dbfc7ca31156419dd4f2094abf94d1ddadd05f
Alex <byhtada@gmail.com>
2019-09-17 17:51:36 +0300

    before

--- js/main.js
+++ js/main.js
@@ -983,6 +983,9 @@ $( document ).ready(function() {
     });
 
 
+
+
+
     function shuffle(a) {
         for (let i = a.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
